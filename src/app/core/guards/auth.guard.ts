20d9af9cd83fb5049313b3709367e50f717716d6
Sutthiporn Srisawad <sutthiporn.s2540@gmail.com>
2025-12-07 10:31:08 +0700

    update

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
-export const authGuard: CanActivateFn = async (route, state) => {
+export const authGuard: CanActivateFn = async () => {
     const authService = inject(AuthService);
 
     if (authService.isLoggedIn) {
         return true;
     }
 
-    await authService.login(window.location.origin + state.url);
+    window.location.href = 'https://portal.sutthiporn.dev';
     return false;
 };
