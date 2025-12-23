6a241df6287e71f5d15bb60a11efe1c05595e105
Sutthiporn Srisawad <sutthiporn.s2540@gmail.com>
2025-12-23 23:38:35 +0700

    update

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -5,20 +5,25 @@ import { AuthService } from '../auth/auth.service';
 export const authGuard: CanActivateFn = async () => {
     const authService = inject(AuthService);
 
+    console.log('[AuthGuard] Checking Token...');
     if (authService.isLoggedIn) {
+        console.log('[AuthGuard] User is logged in. Clearing sso_checked.');
         sessionStorage.removeItem('sso_checked');
         return true;
     }
 
     const ssoChecked = sessionStorage.getItem('sso_checked');
     const now = Date.now();
+    console.log('[AuthGuard] Not logged in. sso_checked:', ssoChecked);
 
     if (!ssoChecked || (now - parseInt(ssoChecked, 10) > 30000)) {
+        console.log('[AuthGuard] Starting login flow (first try or timeout).');
         sessionStorage.setItem('sso_checked', now.toString());
         await authService.login();
         return false;
     }
 
+    console.log('[AuthGuard] Login flow already attempted. Redirecting to Portal.');
     sessionStorage.removeItem('sso_checked');
     window.location.href = 'https://portal.sutthiporn.dev';
     return false;
